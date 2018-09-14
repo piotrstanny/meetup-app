6c3a0a32cb6639371a723ba85b7b0fbc5e57eae3
PiterSt <piotr.stanny@gmail.com>
2018-09-14 12:01:10 +0200

    Toolbar and navigation drawer skeleton created.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <v-app>
-    
+    <v-toolbar>
+      <v-toolbar-side-icon @click="sideNav = !sideNav"></v-toolbar-side-icon>
+      <v-toolbar-title>Trip Meetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>perm_media</v-icon>
+          View Meetups
+          </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-navigation-drawer v-model="sideNav">
+    </v-navigation-drawer>
+    <main>
+    </main>
   </v-app>
 </template>
 
@@ -8,6 +22,7 @@
 export default {
   data () {
     return {
+      sideNav: false
     }
   },
   name: 'App'
