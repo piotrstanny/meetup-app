bdb681594f540b5f510bf63389f6ad1a90923313
PiterSt <piotr.stanny@gmail.com>
2018-10-13 16:22:02 +0100

    featured Meetups loading from Vuex store

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -33,24 +33,15 @@
 
 <script>
     export default {
-        // eslint-disable-next-line
-        data () {
-            // eslint-disable-next-line
-            return {
-                // eslint-disable-next-line
-                meetups: [
-                    {imageUrl: 'https://3.bp.blogspot.com/-BSH0BrwZdiI/VyYNspLlLMI/AAAAAAAAIaU/DnyPstUOU1AKF186zhphr7jvrFxPpmwXwCLcB/s1600/zmarz%25C5%2582e%2Bczuby.jpg', id: 'wwgrrege', title: 'Tatry Mountain Trip'},
-                    {imageUrl: 'https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F11c28fb2-133d-11e8-9ac6-bbf931a203ee.jpg', id: 'wwgvwre', title: 'Mount Blanc Trip'}
-                ]
+        computed: {
+            meetups () {
+              return this.$store.getters.featuredMeetups
             }
         },
-        // eslint-disable-next-line
         methods: {
-            // eslint-disable-next-line
-            onLoadMeetup (id) {
-                // eslint-disable-next-line
-                this.$router.push(`/meetups/${id}`)
-            }
+          onLoadMeetup (id) {
+            this.$router.push(`/meetups/${id}`)
+          }
         }
     }
 </script>
