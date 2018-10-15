08127ff50a1eada2ccac517488242b0ce34ef01f
PiterSt <piotr.stanny@gmail.com>
2018-10-16 00:40:32 +0100

    SignUserUp function created in th store. OnSignup method dispatching data.

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -5,7 +5,7 @@
         <v-card>
           <v-card-text>
             <v-container>
-              <v-form>
+              <v-form @submit.prevent="onSignup">
                 <v-layout row>
                   <v-flex xs12>
                     <v-text-field
@@ -91,12 +91,7 @@ export default {
   },
   methods: {
     onSignup () {
-      // Vuex
-      console.log({
-        email: this.email,
-        password: this.password,
-        confirmPassword: this.confirmPassword
-      })
+      this.$store.dispatch('signUserUp', {email: this.email, password: this.password})
     }
   }
 }
