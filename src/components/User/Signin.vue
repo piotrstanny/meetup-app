96c3085e405d47331dfe5ff61fbf6a56ec77a3c5
PiterSt <piotr.stanny@gmail.com>
2018-10-16 23:27:35 +0100

    Sign in form working properly.

--- src/components/User/Signin.vue
+++ src/components/User/Signin.vue
@@ -1,5 +1,90 @@
 <template>
-    <div>
-        <p>The Sign in Page</p>
-    </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <v-form @submit.prevent="onSignin">
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                    name="email"
+                    id="email"
+                    label="E-mail"
+                    type="email"
+                    v-model="email"
+                    :rules="emailRules"
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                    name="password"
+                    id="password"
+                    label="Password"
+                    type="password"
+                    v-model="password"
+                    :rules="passwordRules"
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn
+                    type="submit"
+                    large
+                    class="warning mt-5"
+                    color="black--text"
+                    :disabled="!signupIsValid"
+                    >Sign In</v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required'
+      ]
+    }
+  },
+  computed: {
+    signupIsValid () {
+      return this.email !== '' &&
+      this.password !== ''
+    },
+    user () {
+      return this.$store.getters.user
+    }
+  },
+  watch: {
+    user (value) {
+      if (value !== null && value !== undefined) {
+        this.$router.push('/')
+      }
+    }
+  },
+  methods: {
+    onSignin () {
+      this.$store.dispatch('signUserIn', {email: this.email, password: this.password})
+    }
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -66,6 +66,21 @@ export const store = new Vuex.Store({
           console.log(error)
         }
       )
+    },
+    signUserIn ({commit}, payload) {
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+      .then(
+        user => {
+          const newUser = {
+            id: user.uid,
+            registeredMeetups: []
+          }
+          commit('setUser', newUser)
+        }
+      ).catch(error => {
+        console.log(error)
+      }
+      )
     }
   },
   getters: {
