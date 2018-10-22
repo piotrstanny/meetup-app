8c8f0b11f74aa3fa594977696f6ca23f5275e649
PiterSt <piotr.stanny@gmail.com>
2018-10-22 15:32:55 +0100

    Edit meetup modal buttons: Validation implemented and closing modal action.

--- src/components/Meetup/Edit/EditMeetupDialog.vue
+++ src/components/Meetup/Edit/EditMeetupDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog max-width="350">
+  <v-dialog max-width="350" persistent v-model="editDialog">
     <v-btn fab small slot="activator" color="error"><v-icon>edit</v-icon></v-btn>
     <v-card>
       <v-container>
@@ -17,12 +17,14 @@
             id="title"
             label="Title"
             v-model="editedTitle"
+            :rules="titleRules"
             ></v-text-field>
             <v-textarea
             name="description"
             id="description"
             label="Description"
             v-model="editedDescription"
+            :rules="descriptionRules"
             multi-line
             ></v-textarea>
           </v-flex>
@@ -30,9 +32,9 @@
         <v-layout row wrap>
           <v-flex xs12>
             <v-card-actions>
-              <v-btn flat class="grey--text">Cancel</v-btn>
+              <v-btn flat class="grey--text" @click="editDialog = false">Cancel</v-btn>
               <v-spacer></v-spacer>
-              <v-btn class="success black--text">Save</v-btn>
+              <v-btn class="success black--text" @click="onSaveChanges">Save</v-btn>
             </v-card-actions>
           </v-flex>
         </v-layout>
@@ -46,8 +48,23 @@ export default {
   props: ['meetup'],
   data () {
     return {
+      descriptionRules: [
+        v => !!v || 'Description is required'
+      ],
+      titleRules: [
+        v => !!v || 'Title is required'
+      ],
+      editDialog: false,
       editedTitle: this.meetup.title,
       editedDescription: this.meetup.description
+    }
+  },
+  methods: {
+    onSaveChanges () {
+      if (this.editedTitle.trim() === '' || this.editedDescription.trim() === '') {
+        return
+      }
+      this.editDialog = false
     }
   }
 }
